e43c78fafc9f81f24d2ea177a16b74f89d893e57
ro1990Uy <43139111+ro1990Uy@users.noreply.github.com>
2018-10-20 20:39:11 -0300

    v1

--- primary-source/src/main/java/com/uy/agencia/AgenciaImpl.java
+++ primary-source/src/main/java/com/uy/agencia/AgenciaImpl.java
@@ -9,6 +9,8 @@ import javax.naming.InitialContext;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class AgenciaImpl {
     private static AgenciaImpl ourInstance = new AgenciaImpl();
@@ -30,14 +32,34 @@ public class AgenciaImpl {
                 InitialContext initialContext = new InitialContext();
                 DataSource ds= (DataSource) initialContext.lookup("Java/AgenciaDS");
                 Connection connection = ds.getConnection();
-                PreparedStatement ps = connection.prepareStatement();
+                PreparedStatement ps = connection.prepareStatement("INSERT INTO `agencia`.`tickets_estacionamiento` (`idtickets_estacionamiento`,`fecha_venta`,`Hora_inicio`,`matricula`,`importe`,`minutos`) VALUES ('"+tiket.getNumeroTicket()+"','"+fechaaVenta+ "','"+inicioEst+"','"+matricula+"'," +tiket.getImporte()+","+tiket.getMinutos()+");");
                 ps.execute();
-
-
             }else {throw new AgenciaImplException("NO SE PUDEO VENDER EL TICKET ");}
         }catch (Exception e){
 
         }
         return tiket;
+    }
+    public String anulaTicket(int id){
+        String rsp=null;
+        try {
+        InitialContext initialContext=new InitialContext();
+        DataSource ds= (DataSource) initialContext.lookup("Java/AgenciaDS");
+        Connection connection = ds.getConnection();
+        PreparedStatement preparedStatement=connection.prepareStatement("SELECT  * FROM `agencia`.`tickets_estacionamiento` WHERE `idtickets_estacionamiento`="+id+";");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if(resultSet.next()){
+            preparedStatement=connection.prepareStatement("DELETE  FROM  WHERE `idtickets_estacionamiento`="+id+";");
+            preparedStatement.execute();
+            WebServiceImm_Service ws = new WebServiceImm_Service();
+            WebServiceImm webServiceImm=ws.getWebServiceImmPort();
+            rsp= webServiceImm.bajaTiket(id,"abitab");
+        }else {
+            throw new TicketExeption("no se encoontro el tiket en la agencia");
+        }
+        } catch (Exception e) {
+            e.getMessage();
+        }
+        return rsp;
     }
 }
---
+++ primary-source/src/main/java/com/uy/agencia/TicketExeption.java
@@ -0,0 +1,7 @@
+package com.uy.agencia;
+
+public class TicketExeption extends Exception{
+    public TicketExeption(String message) {
+        super(message);
+    }
+}
